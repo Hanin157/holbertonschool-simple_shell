39f13525b9a4e94e4d545efa5a31a9651b8e40a5
lilsouy <solaff.alessa@gmail.com>
2025-12-31 19:55:20 +0000

    fix

--- shell.c
+++ shell.c
@@ -1,8 +1,8 @@
 #include <unistd.h>    /* fork, execve */
 #include <sys/wait.h>  /* waitpid */
-#include <stdlib.h>    /* malloc, free, exit */
-#include <stdio.h>     /* perror, printf */
-#include <string.h>    /* strlen, strtok */
+#include <string.h>    /* strlen */
+#include <stdlib.h>    /* malloc, free */
+#include <stdio.h>     /* perror */
 #include "shell.h"
 
 /**
@@ -76,25 +76,14 @@ char *trim_spaces(char *str)
 }
 
 /**
-	* execute_command - forks and executes a command with optional arguments
-	* @line: command line string
+	* execute_command - forks and executes a single command
+	* @line: command to execute
 	*/
 void execute_command(char *line)
 {
 	pid_t pid;
 	int status;
-	char *args[1024]; /* max 1024 arguments */
-	char *token;
-	int i = 0;
-
-	/* Split line into command + arguments */
-	token = strtok(line, " ");
-	while (token != NULL && i < 1023)
-	{
-	args[i++] = token;
-	token = strtok(NULL, " ");
-	}
-	args[i] = NULL;
+	char *args[2];
 
 	pid = fork();
 	if (pid == -1)
@@ -105,7 +94,10 @@ void execute_command(char *line)
 
 	if (pid == 0)
 	{
-	if (execve(args[0], args, NULL) == -1)
+	args[0] = line;
+	args[1] = NULL;
+
+	if (execve(line, args, NULL) == -1)
 	{
 	perror("./shell");
 	exit(EXIT_FAILURE);
@@ -116,41 +108,3 @@ void execute_command(char *line)
 	waitpid(pid, &status, 0);
 	}
 }
-
-/**
-	* main - entry point for the simple shell
-	*
-	* Return: 0 on success
-	*/
-int main(void)
-{
-	char *line;
-	char *trimmed_line;
-
-	while (1)
-	{
-	/* Print prompt only if input is from terminal */
-	if (isatty(STDIN_FILENO))
-	{
-	write(1, "#cisfun$ ", 9);
-	fflush(stdout);
-	}
-
-	line = read_input();
-	if (line == NULL)
-	break;
-
-	trimmed_line = trim_spaces(line);
-	if (trimmed_line == NULL)
-	{
-	free(line);
-	continue;
-	}
-
-	execute_command(trimmed_line);
-	free(line);
-	}
-
-	return (0);
-}
-
