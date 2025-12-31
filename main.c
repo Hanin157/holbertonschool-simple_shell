c02b5d6556c2fe3017cc1dde8fa590357c2b365a
lilsouy <solaff.alessa@gmail.com>
2025-12-31 15:59:22 +0000

    the header and the main files

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+	* main - entry point for the simple shell
+	* @argc: argument count
+	* @argv: argument vector
+	*
+	* Return: Always 0
+	*/
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv);
+
+#endif /* SHELL_H */
