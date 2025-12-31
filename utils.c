4158d541e85fe87dfe58a79039db8139c9be0a87
lilsouy <solaff.alessa@gmail.com>
2025-12-31 17:02:41 +0000

    fixing the utlis file

--- utils.c
+++ utils.c
@@ -1,26 +1,34 @@
 #include "shell.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
-	* trim_whitespace - removes leading/trailing whitespace
+	* trim_whitespace - removes leading and trailing whitespace
 	* @str: string to trim
-	* Return: trimmed string
+	*
+	* Return: pointer to trimmed string
 	*/
 char *trim_whitespace(char *str)
 {
 	char *end;
 
-	while (isspace((unsigned char)*str))
+	if (str == NULL)
+	return (NULL);
+
+	/* Trim leading space */
+	while (*str && isspace((unsigned char)*str))
 	str++;
 
-	if (*str == 0)
-	return str;
+	if (*str == '\0') /* All spaces */
+	return strdup("");
 
+	/* Trim trailing space */
 	end = str + strlen(str) - 1;
 	while (end > str && isspace((unsigned char)*end))
 	end--;
 
 	*(end + 1) = '\0';
 
-	return str;
+	return strdup(str);
 }
-
